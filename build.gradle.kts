0c895b4e933e5b2ccee196869a1e72ab582a43c5
홍시은 <tjwns53@naver.com>
2025-11-21 14:28:04 +0900

    chore: 스웨거 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,9 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // swagger
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
 }
 
 kotlin {
