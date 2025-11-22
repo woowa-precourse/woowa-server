acb22e2718ba31f9d1bf08452b894574fd7f4c36
홍시은 <tjwns53@naver.com>
2025-11-22 19:45:19 +0900

    feat: 코디 관련 repository 작성 (#6)

--- src/main/kotlin/org/woowa/weathercodi/outfit/domain/OutfitClothesRepository.kt
+++ src/main/kotlin/org/woowa/weathercodi/outfit/domain/OutfitClothesRepository.kt
@@ -1,4 +1,7 @@
 package org.woowa.weathercodi.outfit.domain
 
 interface OutfitClothesRepository {
+    fun save(outfitClothes: OutfitClothes): OutfitClothes
+    fun findByOutfitId(outfitId: Long): List<OutfitClothes>
+    fun deleteAllByOutfitId(outfitId: Long)
 }
--- src/main/kotlin/org/woowa/weathercodi/outfit/domain/OutfitRepository.kt
+++ src/main/kotlin/org/woowa/weathercodi/outfit/domain/OutfitRepository.kt
@@ -1,4 +1,8 @@
 package org.woowa.weathercodi.outfit.domain
 
 interface OutfitRepository {
+    fun save(outfit: Outfit): Outfit
+    fun findAllByUserId(userId: Long): List<Outfit>
+    fun findById(id: Long): Outfit?
+    fun delete(outfit: Outfit)
 }
